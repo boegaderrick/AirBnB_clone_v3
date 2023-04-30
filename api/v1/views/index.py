a0b15e5542c2e61b0440fc0b06dc96530bf6e0b7
Derrick Boega Nyabuto <boegaderrick@gmail.com>
2023-05-01 00:27:56 +0300

    flask app - AirBnB object stats

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -7,3 +7,24 @@ from api.v1.views import app_views
 def status():
     """This function shows the status of the API"""
     return {'status': 'OK'}
+
+
+@app_views.route('/stats', methods=['GET'])
+def stats():
+    """This method retrieves object stats"""
+    from models import storage
+    from models.amenity import Amenity
+    from models.city import City
+    from models.place import Place
+    from models.review import Review
+    from models.state import State
+    from models.user import User
+    objs = {}
+    objs['amenities'] = storage.count(Amenity)
+    objs['cities'] = storage.count(City)
+    objs['places'] = storage.count(Place)
+    objs['reviews'] = storage.count(Review)
+    objs['states'] = storage.count(State)
+    objs['users'] = storage.count(User)
+
+    return objs
