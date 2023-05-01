bf2fdb9375564b5cbcd95a77183b4e284324d1c5
Derrick Boega Nyabuto <boegaderrick@gmail.com>
2023-05-01 17:53:21 +0300

    flask - State object API

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """This module defines an API for state objects in storage"""
-from flask import abort, request, make_response
+from flask import abort, request, make_response, jsonify
 from api.v1.views import app_views
 from models import storage
 from models.state import State
@@ -13,7 +13,7 @@ def get_states():
         state objects in storage
     """
     states = [state.to_dict() for state in storage.all(State).values()]
-    return states
+    return jsonify(states)
 
 
 @app_views.route('/states/<state_id>', strict_slashes=False, methods=['GET'])
@@ -25,7 +25,7 @@ def get_state(state_id):
     state = storage.get(State, state_id)
     if not state:
         abort(404)
-    return state.to_dict()
+    return jsonify(state.to_dict())
 
 
 @app_views.route('/states/<state_id>', strict_slashes=False,
@@ -37,7 +37,7 @@ def delete_state(state_id):
         abort(404)
     state.delete()
     storage.save()
-    return make_response({}, 200)
+    return make_response(jsonify({}), 200)
 
 
 @app_views.route('/states', strict_slashes=False, methods=['POST'])
@@ -55,7 +55,7 @@ def create_state():
 
     state = State(name=request.get_json().get('name'))
     state.save()
-    return make_response(state.to_dict(), 201)
+    return make_response(jsonify(state.to_dict()), 201)
 
 
 @app_views.route('/states/<state_id>', strict_slashes=False, methods=['PUT'])
@@ -75,4 +75,4 @@ def update_state(state_id):
         if key != 'id' and key != 'created_at' and key != 'updated_at':
             setattr(state, key, value)
     state.save()
-    return make_response(state.to_dict(), 200)
+    return make_response(jsonify(state.to_dict()), 200)
